d5097bf6e16a4d48924c3e9d8b9213f3f376a9ff
seyniss <shepunk2@kbu.ac.kr>
2025-08-11 14:51:28 +0900

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section>Home</section>
+    <section>
+      <h1>Home</h1>
+      <p>Route 실습을 위한 페이지</p>
+    </section>
   )
 }
 
